36772361b963fe9d64abd22c44a619a979aac133
yangjunhuai <yangjunhuai@bytedance.com>
2023-01-31 12:54:27 +0800

    update sh

--- syncSnippets.sh
+++ syncSnippets.sh
@@ -5,15 +5,15 @@ function mkDirAbsolutely {
 }
 
 # 从云端下载代码块
-function downloadCodesnippetsFromGit {
-    git clone $1
-}
+# function downloadCodesnippetsFromGit {
+#     git clone $1
+# }
 
 # 先给 Xcode 的代码块的文件夹设置权限；
 # 777 和 775 分别代表三种用户身份的文件权限。（文件所有者、文件所属组、其他用户）7=4+2+1 可读可写可执行；6=4+2可读可写；5=1+4；可读可执行
 function moveCodeSnippetsToXcodeSnippetsFolder {
     chmod 775 ~/Library/Developer/Xcode/UserData/CodeSnippets/
-    mv -f ./snippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets/
+    cp -a ./snippets/* ~/Library/Developer/Xcode/UserData/CodeSnippets/
 }
 
 # 先给 Xcode 的 File templates 文件夹设置权限
@@ -34,7 +34,7 @@ function moveFileTemplatesToXcodeFileTemplatesFolder {
     cd $basepath
 
     # 将从云端下载下来的代码模版移动到Xcode系统的模版文件夹下
-    mv -f ./filetemplete/Custom/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/Custom/
+    cp -a ./filetemplete/Custom/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/Custom/
 }
 
 # 强制删除文件夹
@@ -49,25 +49,30 @@ EOF
 
 function syncXcodesnippetsAndFileTemplatesFromOnlinestorage {
     #1
-    mkDirAbsolutely CodeSinppetsAndFileTemplates
+    # mkDirAbsolutely CodeSinppetsAndFileTemplates
     #2
-    cd CodeSinppetsAndFileTemplates
+    # cd CodeSinppetsAndFileTemplates
     #3
-    downloadCodesnippetsFromGit git@github.com:FantasticLBP/codesnippets.git
+    # downloadCodesnippetsFromGit git@github.com:FantasticLBP/codesnippets.git
     #4
-    cd codesnippets
+    # cd codesnippets
+    
+    # 先备份一下
+    cp -a ~/Library/Developer/Xcode/UserData/CodeSnippets/ ./Backup/CodeSnippets
+    cp -a /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/File\ Templates/ ./Backup/FileTemplates
+
     #5
     mkDirAbsolutely ~/Library/Developer/Xcode/UserData/CodeSnippets/
     #6
     moveCodeSnippetsToXcodeSnippetsFolder
     #7
-    moveFileTemplatesToXcodeFileTemplatesFolder
-    #8
-    cd ..
-    #9
-    cd ..
-    #10
-    deleteTheFolderAbsolutely CodeSinppetsAndFileTemplates
+    # moveFileTemplatesToXcodeFileTemplatesFolder
+    # #8
+    # cd ..
+    # #9
+    # cd ..
+    # #10
+    # deleteTheFolderAbsolutely CodeSinppetsAndFileTemplates
 }
 
 syncXcodesnippetsAndFileTemplatesFromOnlinestorage
